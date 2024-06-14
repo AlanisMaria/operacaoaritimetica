caebac91c1fe81081d7839dded711ce67f5504f8
Alanis <160151740+AlanisMaria@users.noreply.github.com>
2024-06-14 12:32:51 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+/*var a = 10;
+var b = 15;
+
+var c = a + b;
+
+console.log(c);
+
+var a = 10;
+var b = 15;
+
+var c = a - b;
+
+console.log(c);
+
+var a = 10;
+var b = 15;
+
+var c = a * b;
+
+console.log(c);
+
+var a = 10;
+var b = 15;
+
+var c = a / b;
+
+console.log(c);
+
+var a = 10;
+var b = 15;
+
+var c = a % b;
+
+console.log(c);
+
+var a = 10;
+var b = 15;
+
+var c = a ++ b;
+
+console.log(c);
+
+var a = 10;
+var b = 15;
+
+var c = a -- b;
+
+console.log(c);*/
